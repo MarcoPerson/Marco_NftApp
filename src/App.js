783700234099f6904c1d05cd0672bf536449b23e
Merveilles AGBETI-MESSAN <marcoperson2000@gmail.com>
2022-08-23 18:43:04 +0200

    Structure and assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Download, Features, SectionWrapper } from "./components"
+import assets from "./assets"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <SectionWrapper
+        title="Trade NFTs, Get Rewards"
+        description="Buy NFTs (or sell 'em) to earn rewards.
+      Explore the market to get started. Join 10+ million people using Marco NFT"
+        showBtn
+        mockupImage={assets.homeHero}
+        banner="bannerr"
+        textBlack
+      />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of Marco NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImage={assets.homeCards}
+        reverse
+        textBlack
+      />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="Marco NFT App is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
+        mockupImage={assets.feature}
+        reverse
+        textBlack
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImage={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+    </>
+  )
 }
 
-export default App;
+export default App
